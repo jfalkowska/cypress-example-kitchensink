f467d648bfa5937adc0a92214424e1d5f3a2d873
Joanna Falkowska <jfalkowska@gmail.com>
2023-09-13 15:42:25 +0200

    Add environment variable to cypress.config.js file

--- cypress/e2e/2-advanced-examples/2nd_cypress_testing_library.cy.js
+++ cypress/e2e/2-advanced-examples/2nd_cypress_testing_library.cy.js
@@ -1,9 +1,11 @@
-context('1st test', () => {
+const navbarText = Cypress.env('navbarText');
+
+context('cypress-testing-library 1st test', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
   it('finds an element by its text', () => {
-    cy.findByText('cypress.io').should('exist');
+    cy.findByText(navbarText).should('exist');
   })
 })
