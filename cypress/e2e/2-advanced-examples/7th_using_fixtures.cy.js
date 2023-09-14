fbb8f122b3c60bddfcd86c78063617ee7d690791
Joanna Falkowska <jfalkowska@gmail.com>
2023-09-14 16:55:11 +0200

    Using fixtures in network requests

--- cypress/e2e/2-advanced-examples/7th_using_fixtures.cy.js
+++ cypress/e2e/2-advanced-examples/7th_using_fixtures.cy.js
@@ -1,30 +1,30 @@
 context('using fixtures', () => {
-    beforeEach(() => {
-        cy.fixture('example').then(function (data) {
-            this.data = data
-            cy.log('THIS: ', this.data)
-        })
+  beforeEach(() => {
+    cy.fixture('example').then(function (data) {
+      this.data = data
+      cy.log('THIS: ', this.data)
     })
+  })
     
-    it('pulls data from a fixture', () => {
-        cy.fixture('example').then((data) => {
-            cy.log('DATA: ', data)
-        })
+  it('pulls data from a fixture', () => {
+    cy.fixture('example').then((data) => {
+      cy.log('DATA: ', data)
     })
+  })
 
-    it('updates fixture data inline', () => {
-        cy.fixture('example').then((data) => {
-            data.email = ('updated@mail.com')
-            cy.log('UPDATED: ', data.email)
-        })
+  it('updates fixture data inline', () => {
+    cy.fixture('example').then((data) => {
+      data.email = ('updated@mail.com')
+      cy.log('UPDATED: ', data.email)
     })
+  })
 
-    it('uses fixture data in a network request', function () {
-        cy.visit('/commands/network-requests')
-        cy.intercept('GET', '**/comments/*', this.data).as('getComments')
-        cy.get('.network-btn').click()
-        cy.wait('@getComments').then((res) => {
-            cy.log('RESPONSE: ', res)
-        })
+  it('uses fixture data in a network request', function () {
+    cy.visit('/commands/network-requests')
+    cy.intercept('GET', '**/comments/*', this.data).as('getComments')
+    cy.get('.network-btn').click()
+    cy.wait('@getComments').then((res) => {
+      cy.log('RESPONSE: ', res)
     })
+  })
 })
