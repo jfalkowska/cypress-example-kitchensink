c1e202715c6ccecc0bbfb92df643af55c5b22f26
Joanna Falkowska <jfalkowska@gmail.com>
2023-09-14 20:55:43 +0200

    Using custom commands

--- cypress/e2e/2-advanced-examples/8th_using_custom_commands.cy.js
+++ cypress/e2e/2-advanced-examples/8th_using_custom_commands.cy.js
@@ -1,15 +1,15 @@
 const token = 'abcd1234'
 
 context('using custom commands', () => {
-    it('set and retrieve a token in local storage', () => {
-        cy.setLocalStorage('token', token)
-        cy.getLocalStorage('token').should('eq', token)
-    })
+  it('set and retrieve a token in local storage', () => {
+    cy.setLocalStorage('token', token)
+    cy.getLocalStorage('token').should('eq', token)
+  })
 
-    it('overwrites the type command by using sensitive characters', () => {
-        cy.visit('/commands/actions')
-        cy.findByPlaceholderText('Email').type('test@email.com')
+  it('overwrites the type command by using sensitive characters', () => {
+    cy.visit('/commands/actions')
+    cy.findByPlaceholderText('Email').type('test@email.com')
         .clear().should('have.value', '')
-        cy.findByPlaceholderText('Email').type('test@email.com', { sensitive: true })
-    })
+    cy.findByPlaceholderText('Email').type('test@email.com', { sensitive: true })
+  })
 })
