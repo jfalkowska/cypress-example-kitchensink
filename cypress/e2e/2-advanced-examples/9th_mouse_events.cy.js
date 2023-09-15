0e52b9cc1fe586a11e9ccb137510ceceeb9a20c2
Joanna Falkowska <jfalkowska@gmail.com>
2023-09-15 11:45:07 +0200

    Using mouse events

---
+++ cypress/e2e/2-advanced-examples/9th_mouse_events.cy.js
@@ -0,0 +1,31 @@
+context('mouse events', () => {
+    beforeEach(() => {
+        cy.visit('/commands/actions')
+    })
+
+    it('trigger a popover on click', () => {
+        cy.get('.action-btn').click()
+        cy.findByText('This popover shows up on click').should('be.visible')
+    })
+
+    it('click on different sections of a canvas', () => {
+        cy.get('#action-canvas').click('top')
+        cy.get('#action-canvas').click('bottomRight')
+        cy.get('#action-canvas').click(30, 100)
+    })
+
+    it('double click', () => {
+        cy.get('.action-div').dblclick().should('not.be.visible')
+        cy.get('.action-input-hidden').should('be.visible')
+    })
+
+    it('right click', () => {
+        cy.get('.rightclick-action-div').rightclick().should('not.be.visible')
+        cy.get('.rightclick-action-input-hidden').should('be.visible')
+    })
+
+    it('hover', () => {
+        cy.get('.dropdown-toggle').trigger('mouseover')
+        cy.get('.dropdown-menu').should('be.visible')
+    })
+})
